a2fc7224246a86a3d324911bfdccd0c6901282f1
Vito Belgiorno-Zegna <bz.vito@gmail.com>
2015-03-12 09:21:07 +1100

    Nearly there. can't figure out how to res.write(properly)

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-// var http = require('http');
+var http = require('http');
 var request = require('request');
 
 var api = 'http://codefight.davidbanham.com/';
 var socials = ['twitter', 'facebook', 'instagram'];
-var object = {};
 
-socials.forEach(function(social) {
-	request(api + social, function (error, response, body) {
-  	if (!error && response.statusCode == 200) {
-    	object[social] = JSON.parse(body);
-    	console.log(object);
-  	}
-	})
-});
-
-// server.listen(3000);
+http.createServer(function(req, res) {
+	var object = {};	
+	socials.forEach(function(social) {
+		request(api + social, function (error, response, body) {
+	  	if (!error && response.statusCode == 200) {
+	    	object[social] = JSON.parse(body);
+	    	console.log(object);
+	  	}
+		})
+	}, function(data) {
+			return data.on('data', function(chunk) {
+				res.writeHead(200, { 'Content-Type': 'application/json' });
+				return res.end(JSON.stringify(object));
+			});
+		}
+	);
+})
+	.listen(3000);
