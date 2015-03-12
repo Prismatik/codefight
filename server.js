4112adf6f2ffec2e615b8815e8e4cbfa16c508ef
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-03-12 18:36:39 +1100

    tried piping

--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-var http = require('http');
-var request = require('request');
+var http = require('http'),
+    bl = require('bl'),
+    request = require('request'),
+    url = 'http://codefight.davidbanham.com/',
+    storage = {},
+    socialArray = ['twitter', 'facebook', 'instagram'];
 
 http.createServer(function(req, res) {
 
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Access-Control-Allow-Origin' : '*'
+  socialArray.forEach(function(company) {
+    http.get(url + company, function callback(response) {
+      console.log(company);
+      response.pipe(bl(function (err, data) {
+        if (err) {
+          return console.error(err)
+        }
+          storage[company] = data.toString();
+          console.log(storage);
+      }))
+    });
   });
 
-  socialArray = ["twitter", "facebook", "instagram"]
-
-  for(var i = 0; i <= 2; i++) {
-    var company = socialArray[i];
-    var jsonObject = {}
-
-  request('http://codefight.davidbanham.com/' + company, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        jsonObject[company] = info;
-        console.log(jsonObject);
-      }
-    });
-  }
-
-    
 }).listen(3000);
 
 console.log('Sorry about the delay - Please Standby for info to come through');
+
